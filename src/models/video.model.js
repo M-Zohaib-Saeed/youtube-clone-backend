7f588e8d4d1fb3758e38666dbe90877cd6c8d083
M-Zohaib-Saeed <mianzohaibsaeed786@gmail.com>
2026-06-25 03:06:07 +0500

    Add User and Video model

---
+++ src/models/video.model.js
@@ -0,0 +1,46 @@
+import mongoose , {Schema} from "mongoose";
+import mongooseAggregatePaginate from "mongoose-paginate-v2";
+
+const videoSchema = new Schema({
+    videoFile: {
+        type: String, // cloudinary url
+        required: true,
+    },
+    thumbnail: {
+        type: String, // cloudinary url
+        required: true,
+    },
+    title: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    duration: {
+        type: Number, // duration in seconds
+        required: true,
+    },
+    views: {
+        type: Number,
+        default: 0,
+    },
+    ispublished: {
+        type: Boolean,
+        default: true,
+    },
+    owner: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    }
+},
+    {
+        timestamps: true,
+    }
+);
+
+videoSchema.plugin(mongooseAggregatePaginate);
+
+export const Video = mongoose.model("Video", videoSchema);
